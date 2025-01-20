d7b14f295a22ca5b55443d4d75f80ece78c7b9c3
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-20 20:47:25 +0100

    create: description overview area

--- src/app/main-content-landingpage/projects-overview-section/description-overview/description-overview.component.ts
+++ src/app/main-content-landingpage/projects-overview-section/description-overview/description-overview.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IncreaseWidthRepeatDirective } from '../../../shared/directive/increase-width-repeat.directive';
 
 @Component({
   selector: 'app-description-overview',
   standalone: true,
-  imports: [],
+  imports: [IncreaseWidthRepeatDirective],
   templateUrl: './description-overview.component.html',
   styleUrl: './description-overview.component.scss'
 })
