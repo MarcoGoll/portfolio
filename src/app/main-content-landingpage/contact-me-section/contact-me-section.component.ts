d22301392174c35868dfd0bd6df1deb2e8157c78
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-22 07:23:56 +0100

    create: basic html for contactMeSection

--- src/app/main-content-landingpage/contact-me-section/contact-me-section.component.ts
+++ src/app/main-content-landingpage/contact-me-section/contact-me-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IncreaseWidthRepeatDirective } from '../../shared/directive/increase-width-repeat.directive';
 
 @Component({
   selector: 'app-contact-me-section',
   standalone: true,
-  imports: [],
+  imports: [IncreaseWidthRepeatDirective],
   templateUrl: './contact-me-section.component.html',
   styleUrl: './contact-me-section.component.scss'
 })
