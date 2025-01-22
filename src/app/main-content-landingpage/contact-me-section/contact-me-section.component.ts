817709b77bb3528e50f45b9b6c116bdf38b8bb07
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-22 09:30:17 +0100

    create: basic html for form

--- src/app/main-content-landingpage/contact-me-section/contact-me-section.component.ts
+++ src/app/main-content-landingpage/contact-me-section/contact-me-section.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { IncreaseWidthRepeatDirective } from '../../shared/directive/increase-width-repeat.directive';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me-section',
   standalone: true,
-  imports: [IncreaseWidthRepeatDirective],
+  imports: [IncreaseWidthRepeatDirective, FormsModule],
   templateUrl: './contact-me-section.component.html',
   styleUrl: './contact-me-section.component.scss'
 })
 export class ContactMeSectionComponent {
 
+  contactData: {
+    name: string;
+    email: string;
+    message: string;
+  } = {
+      name: "",
+      email: "",
+      message: "",
+    }
+
+  onSubmit() {
+    console.log(this.contactData);
+  }
 }
