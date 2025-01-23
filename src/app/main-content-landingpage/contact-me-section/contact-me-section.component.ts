549a14610d5f4bc32c43cf8a483a02cac5373247
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-23 15:04:40 +0100

    create: send mail via php + img size changes

--- src/app/main-content-landingpage/contact-me-section/contact-me-section.component.ts
+++ src/app/main-content-landingpage/contact-me-section/contact-me-section.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IncreaseWidthRepeatDirective } from '../../shared/directive/increase-width-repeat.directive';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-me-section',
@@ -10,6 +11,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './contact-me-section.component.scss'
 })
 export class ContactMeSectionComponent {
+
+  http = inject(HttpClient);
 
   contactData: {
     name: string;
@@ -21,7 +24,36 @@ export class ContactMeSectionComponent {
       message: "",
     }
 
-  onSubmit() {
-    console.log(this.contactData);
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            // here all what should happen after submit
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      // here all what should happen after submit (TESTMODE)
+      console.log('TESTMODE IS ACTIVE: change mailTest to false to change it');
+      ngForm.resetForm();
+    }
   }
 }
