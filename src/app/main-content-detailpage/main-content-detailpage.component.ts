d2435a4ebd2cf6c52eefba11c2bec9ea3d2cf04d
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-26 20:11:41 +0100

    create: detailpage basic HTML+CSS

--- src/app/main-content-detailpage/main-content-detailpage.component.ts
+++ src/app/main-content-detailpage/main-content-detailpage.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProjectDataService } from '../shared/services/project-data.service';
 import { IncreaseWidthRepeatDirective } from '../shared/directive/increase-width-repeat.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-content-detailpage',
   standalone: true,
-  imports: [IncreaseWidthRepeatDirective],
+  imports: [IncreaseWidthRepeatDirective, RouterLink],
   templateUrl: './main-content-detailpage.component.html',
   styleUrl: './main-content-detailpage.component.scss'
 })
