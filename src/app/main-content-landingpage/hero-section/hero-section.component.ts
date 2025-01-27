5304383ab33b15fe496006f4f112934b4da87b90
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-27 14:32:56 +0100

    create: logic to change navlink colour depending on backgroundimage

--- src/app/main-content-landingpage/hero-section/hero-section.component.ts
+++ src/app/main-content-landingpage/hero-section/hero-section.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroHighlightDirective } from '../../shared/directive/hero-highlight.directive';
 import { PolaroidHighlightDirective } from '../../shared/directive/polaroid-highlight.directive';
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [HeroHighlightDirective, PolaroidHighlightDirective],
+  imports: [HeroHighlightDirective, PolaroidHighlightDirective, HeaderComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
