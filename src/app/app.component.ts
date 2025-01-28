e8f044a2dfceef649108479ee3d702f03456d34d
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-28 21:28:05 +0100

    create: translation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/components/header/header.component";
-import { FooterComponent } from "./shared/components/footer/footer.component";
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,18 +3,27 @@ import { RouterLink } from '@angular/router';
 import { ParentFirstChildTopDownDirective } from '../../directive/parent-first-child-top-down.directive';
 import { ParentFirstChildLeftRightDirective } from '../../directive/parent-first-child-left-right.directive';
 import { CommonModule } from '@angular/common';
+import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, ParentFirstChildTopDownDirective, ParentFirstChildLeftRightDirective, CommonModule],
+  imports: [
+    RouterLink,
+    ParentFirstChildTopDownDirective,
+    ParentFirstChildLeftRightDirective,
+    CommonModule,
+    TranslatePipe,
+    TranslateDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   isHamburgerOpen: boolean = false;
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2, private translate: TranslateService) { }
+
 
   async ngAfterViewInit(): Promise<void> {
     const navTextElements = this.elRef.nativeElement.querySelectorAll('.nav-text');
@@ -109,4 +118,9 @@ export class HeaderComponent {
   toggleHamburger() {
     this.isHamburgerOpen = !this.isHamburgerOpen;
   }
+
+  toggleLanguage(language: string) {
+    this.translate.use(language);
+  }
+
 }
