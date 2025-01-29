3dad50068f9b3c3e884a4a1873aa9c40ac7556a1
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-29 14:41:12 +0100

    create: translation for hero-section

--- src/app/main-content-landingpage/hero-section/hero-section.component.ts
+++ src/app/main-content-landingpage/hero-section/hero-section.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HeroHighlightDirective } from '../../shared/directive/hero-highlight.directive';
 import { PolaroidHighlightDirective } from '../../shared/directive/polaroid-highlight.directive';
-
+import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [HeroHighlightDirective, PolaroidHighlightDirective],
+  imports: [HeroHighlightDirective, PolaroidHighlightDirective, TranslatePipe,
+    TranslateDirective],
   templateUrl: './hero-section.component.html',
   styleUrls: ['./hero-section.component.scss', 'hero-section.responsive.scss']
 })
 export class HeroSectionComponent {
-
+  constructor(private translate: TranslateService) { }
 }
