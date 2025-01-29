a655fda90aa2d1a4351cd0b3b4860d88ebd2208b
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-29 14:29:16 +0100

    style: responsive hero-section

--- src/app/main-content-landingpage/hero-section/hero-section.component.ts
+++ src/app/main-content-landingpage/hero-section/hero-section.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroHighlightDirective } from '../../shared/directive/hero-highlight.directive';
 import { PolaroidHighlightDirective } from '../../shared/directive/polaroid-highlight.directive';
-import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [HeroHighlightDirective, PolaroidHighlightDirective, HeaderComponent],
+  imports: [HeroHighlightDirective, PolaroidHighlightDirective],
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+  styleUrls: ['./hero-section.component.scss', 'hero-section.responsive.scss']
 })
 export class HeroSectionComponent {
 
