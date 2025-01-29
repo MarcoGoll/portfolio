facb1c32784782fdee3591e9ba0931d7afdb8e80
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-29 21:11:29 +0100

    stlye: start with responsive/translation aboutMeSection

--- src/app/main-content-landingpage/about-me-section/about-me-section.component.ts
+++ src/app/main-content-landingpage/about-me-section/about-me-section.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { IncreaseWidthRepeatDirective } from '../../shared/directive/increase-width-repeat.directive';
+import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-about-me-section',
   standalone: true,
-  imports: [IncreaseWidthRepeatDirective],
+  imports: [IncreaseWidthRepeatDirective, TranslatePipe,
+    TranslateDirective],
   templateUrl: './about-me-section.component.html',
-  styleUrl: './about-me-section.component.scss'
+  styleUrls: ['./about-me-section.component.scss', './about-me-section.responsive.scss']
 })
 export class AboutMeSectionComponent {
-
+  constructor(private translate: TranslateService) { }
 }
