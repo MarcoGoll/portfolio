0131b5ca76d6eb52d062c87f89a59873604de331
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-30 11:12:43 +0100

    style: responsive/translate skillSection

--- src/app/main-content-landingpage/skill-section/skill-section.component.ts
+++ src/app/main-content-landingpage/skill-section/skill-section.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { IncreaseWidthRepeatDirective } from '../../shared/directive/increase-width-repeat.directive';
+import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
+
 
 @Component({
   selector: 'app-skill-section',
   standalone: true,
-  imports: [IncreaseWidthRepeatDirective],
+  imports: [IncreaseWidthRepeatDirective, TranslatePipe,
+    TranslateDirective],
   templateUrl: './skill-section.component.html',
-  styleUrl: './skill-section.component.scss'
+  styleUrls: ['./skill-section.component.scss', './skill-section.responsive.scss']
 })
 export class SkillSectionComponent {
-
+  constructor(private translate: TranslateService) { }
 }
