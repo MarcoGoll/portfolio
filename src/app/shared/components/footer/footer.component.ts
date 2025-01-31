26c0757803f1eeec4d56cc30ffb5e02bcfe29dc8
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-31 21:48:16 +0100

    style: responsive+translation footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    TranslatePipe,
+    TranslateDirective
+  ],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss', 'footer.responsive.scss']
 })
 export class FooterComponent {
-
+  constructor(private translate: TranslateService) { }
 }
