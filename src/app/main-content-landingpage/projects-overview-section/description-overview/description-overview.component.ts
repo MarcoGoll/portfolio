1276acd2c8c124260f1049f07d093b22ed4d812b
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-02-01 08:02:43 +0100

    style: adjust style within product detailpage

--- src/app/main-content-landingpage/projects-overview-section/description-overview/description-overview.component.ts
+++ src/app/main-content-landingpage/projects-overview-section/description-overview/description-overview.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { IncreaseWidthRepeatDirective } from '../../../shared/directive/increase-width-repeat.directive';
+import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-description-overview',
   standalone: true,
-  imports: [IncreaseWidthRepeatDirective],
+  imports: [IncreaseWidthRepeatDirective, TranslatePipe,
+    TranslateDirective],
   templateUrl: './description-overview.component.html',
   styleUrl: './description-overview.component.scss'
 })
 export class DescriptionOverviewComponent {
-
+  constructor(private translate: TranslateService) { }
 }
