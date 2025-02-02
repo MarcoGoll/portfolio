b8a99730f98af9e9a1f0e750f06a64b2c7faf7b8
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-02-02 09:42:45 +0100

    create: animation for click me sticker

--- src/app/main-content-landingpage/skill-section/skill-section.component.ts
+++ src/app/main-content-landingpage/skill-section/skill-section.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { IncreaseWidthRepeatDirective } from '../../shared/directive/increase-width-repeat.directive';
 import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-skill-section',
   standalone: true,
-  imports: [IncreaseWidthRepeatDirective, TranslatePipe,
+  imports: [CommonModule, IncreaseWidthRepeatDirective, TranslatePipe,
     TranslateDirective],
   templateUrl: './skill-section.component.html',
   styleUrls: ['./skill-section.component.scss', './skill-section.responsive.scss']
 })
 export class SkillSectionComponent {
+  isAnimationstarted: boolean = false;
   constructor(private translate: TranslateService) { }
+
+  startInterestAnimation() {
+    this.isAnimationstarted = true;
+  }
 }
