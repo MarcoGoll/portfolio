07a8f0cb7bed135804feb8e57c28193636e5427a
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-02-03 07:03:37 +0100

    style: animation featured project

--- src/app/main-content-landingpage/projects-overview-section/projekt-overview/projekt-overview.component.ts
+++ src/app/main-content-landingpage/projects-overview-section/projekt-overview/projekt-overview.component.ts
@@ -2,11 +2,15 @@ import { Component, Input } from '@angular/core';
 import { Project } from '../../../shared/interfaces/project';
 import { ProjectHighlightDirective } from '../../../shared/directive/project-highlight.directive';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projekt-overview',
   standalone: true,
-  imports: [ProjectHighlightDirective, RouterLink],
+  imports: [
+    ProjectHighlightDirective,
+    RouterLink,
+    CommonModule],
   templateUrl: './projekt-overview.component.html',
   styleUrls: ['./projekt-overview.component.scss', './projekt-overview.responsive.scss']
 })
