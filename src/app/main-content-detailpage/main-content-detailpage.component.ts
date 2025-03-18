5ea277a049718dd1e696caca01f2277320aa1b13
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-03-18 09:10:25 +0100

    change: cleaner code

--- src/app/main-content-detailpage/main-content-detailpage.component.ts
+++ src/app/main-content-detailpage/main-content-detailpage.component.ts
@@ -5,16 +5,26 @@ import { Router, ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Project } from '../shared/interfaces/project';
-import { TranslatePipe, TranslateDirective, TranslateService } from "@ngx-translate/core";
-
+import {
+  TranslatePipe,
+  TranslateDirective,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-main-content-detailpage',
   standalone: true,
-  imports: [IncreaseWidthRepeatDirective, RouterLink, TranslatePipe,
-    TranslateDirective],
+  imports: [
+    IncreaseWidthRepeatDirective,
+    RouterLink,
+    TranslatePipe,
+    TranslateDirective,
+  ],
   templateUrl: './main-content-detailpage.component.html',
-  styleUrls: ['./main-content-detailpage.component.scss', './main-content-detailpage.responsive.scss']
+  styleUrls: [
+    './main-content-detailpage.component.scss',
+    './main-content-detailpage.responsive.scss',
+  ],
 })
 export class MainContentDetailpageComponent {
   projectData = inject(ProjectDataService);
@@ -24,61 +34,66 @@ export class MainContentDetailpageComponent {
     private route: ActivatedRoute,
     private router: Router,
     private translate: TranslateService
-  ) { }
+  ) {}
 
   /**
-  * Is called when the component is initialized.
-  * Is storing the 'id' parameter in the `urlID` variable.
-  */
+   * Is called when the component is initialized.
+   * Is storing the 'id' parameter in the `urlID` variable.
+   */
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.urlID = params.get('id');
     });
   }
 
   /**
-  * Navigates the user to the landing page (home route).
-  */
+   * Navigates the user to the landing page (home route).
+   */
   gotoLandingpage() {
     this.router.navigate(['/']);
   }
 
   /**
-  * Returns the ID of the next project based on the current index and language.
-  * 
-  * This method iterates through the project list (English or German) and finds the 
-  * next project's ID. If the current project is the last in the list, it returns the 
-  * first project's ID to ensure looping behavior.
-  * 
-  * @param {number} currentIndex - The index of the current project.
-  * @returns {string | undefined} The ID of the next project, or `undefined` if not found.
-  */
+   * Returns the ID of the next project based on the current index and language.
+   *
+   * This method iterates through the project list (English or German) and finds the
+   * next project's ID. If the current project is the last in the list, it returns the
+   * first project's ID to ensure looping behavior.
+   *
+   * @param {number} currentIndex - The index of the current project.
+   * @returns {string | undefined} The ID of the next project, or `undefined` if not found.
+   */
   nextID(currentIndex: number) {
-    if (this.projectData.currentLanguage === "en") {
-      for (let i = 0; i < this.projectData.projectsEN.length; i++) {
-        if (i == this.projectData.projectsEN.length - 1) {
-          return this.projectData.projectsEN[0].id;
-        } else if (this.projectData.projectsEN[i].index == currentIndex) {
-          return this.projectData.projectsEN[i + 1].id;
-        }
-      }
+    if (this.projectData.currentLanguage === 'en') {
+      return this.getNextIDForArray(currentIndex, this.projectData.projectsEN);
     }
-    if (this.projectData.currentLanguage === "de") {
-      for (let i = 0; i < this.projectData.projectsDE.length; i++) {
-        if (i == this.projectData.projectsDE.length - 1) {
-          return this.projectData.projectsDE[0].id;
-        } else if (this.projectData.projectsDE[i].index == currentIndex) {
-          return this.projectData.projectsDE[i + 1].id;
-        }
+    if (this.projectData.currentLanguage === 'de') {
+      return this.getNextIDForArray(currentIndex, this.projectData.projectsDE);
+    }
+  }
+
+  /**
+   * Returns the ID of the next project in the array based on the given current index.
+   * If the current index corresponds to the last element, it returns the first element's ID.
+   *
+   * @param {number} currentIndex - The index of the current project.
+   * @param {Project[]} myArray - The array of projects.
+   * @returns {number | undefined} The ID of the next project, or `undefined` if not found.
+   */
+  getNextIDForArray(currentIndex: number, myArray: Project[]) {
+    for (let i = 0; i < myArray.length; i++) {
+      if (i == myArray.length - 1) {
+        return myArray[0].id;
+      } else if (myArray[i].index == currentIndex) {
+        return myArray[i + 1].id;
       }
     }
   }
 
   /**
-  * Scrolls the window to the top of the page.
-  */
+   * Scrolls the window to the top of the page.
+   */
   resetPosition() {
-    window.scroll({ top: 0 })
+    window.scroll({ top: 0 });
   }
 }
-
